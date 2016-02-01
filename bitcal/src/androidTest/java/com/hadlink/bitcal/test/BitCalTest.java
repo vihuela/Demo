14183fcd77f0cd78ac9727e7018396c852ed14c8
layo <lomoliger@hotmail.com>
2016-02-01 19:15:55 +0800

    update demo

--- bitcal/src/androidTest/java/com/hadlink/bitcal/test/BitCalTest.java
+++ bitcal/src/androidTest/java/com/hadlink/bitcal/test/BitCalTest.java
@@ -26,22 +26,25 @@ public class BitCalTest extends InstrumentationTestCase {
      * 增加、清除标志位
      */
     public void testApply1() {
-        final int x = 0x00000010;
-        final int y = 0X00000001;
+        final int x = 10;
+        final int y = 1;
+        final int mask = 0xF;
         final int res1 = x | y;
         final int res2 = x & x;
         final int res3 = res1 & ~y;//res1中清除y
         final int res4 = res3 | y;//res3附加y
 
-        final boolean isExistX1 = (res1 & x) != 0;//包含x
+        final boolean isExistX1 = (res1 & x) == x;//包含x
+        final boolean isExistX2 = (res1 & x) !=0;//包含x
+        final boolean isExistX3 = (x & mask) == x;//包含x
 
-        final boolean isExistX2 = (res1 & ~y) == x;//包含x
+        final boolean isExistX4 = (res1 & ~y) == x;//包含x
 
 
-        assertEquals(res1, 17);
-        assertEquals(res2, 16);
-        assertEquals(res3, 16);
-        assertEquals(res4, 17);
+        assertEquals(res1, 11);
+        assertEquals(res2, 10);
+        assertEquals(res3, 10);
+        assertEquals(res4, 11);
         assertEquals(isExistX1, isExistX2);
     }
 

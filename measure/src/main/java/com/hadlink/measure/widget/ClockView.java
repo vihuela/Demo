e1523af04237fa433ed3e5d5192c654ca9e0cbd8
layo <lomoliger@hotmail.com>
2016-02-17 15:58:03 +0800

    update clock View demo

--- measure/src/main/java/com/hadlink/measure/widget/ClockView.java
+++ measure/src/main/java/com/hadlink/measure/widget/ClockView.java
@@ -1,13 +1,19 @@
 package com.hadlink.measure.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
+
+import com.hadlink.measure.R;
 
 /**
  * @author Created by lyao on 2016/2/3.
@@ -26,11 +32,26 @@ public class ClockView extends View {
     private Paint textPaint;
     private int canvasCount;
     private int lineLength;
-    private int centerX, centerY;
+    private int mCenterX, mCenterY;
     private Path arrowPath;
+    /**
+     * rotate canvas
+     */
+    private boolean isRotate;
+    private double alpha;
+    private float mTouchX;
+    private float mTouchY;
+    private float mCanvasRotateX = 0;
+    private float mCanvasRotateY = 0;
+    private float mCanvasMaxRotateDegree = 20;
+    private Matrix mMatrix = new Matrix();
+    private Camera mCamera = new Camera();
 
     public ClockView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.clockView);
+        isRotate = typedArray.getBoolean(typedArray.getIndex(R.styleable.clockView_isRotate), false);
+        typedArray.recycle();
         canvasCount = 120;
         contentSize = Math.min(getResources().getDisplayMetrics().heightPixels, getResources().getDisplayMetrics().widthPixels);
         lineLength = getPx(context, 10f);
@@ -65,6 +86,7 @@ public class ClockView extends View {
          */
         arrowCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         arrowCirclePaint.setColor(Color.parseColor("#AFD0E2"));
+
         /**
          * 文字
          */
@@ -93,19 +115,34 @@ public class ClockView extends View {
         /**
          * 中心点
          */
-        centerX = contentX + getPaddingLeft();
-        centerY = contentY + getPaddingTop();
+        mCenterX = contentX + getPaddingLeft();
+        mCenterY = contentY + getPaddingTop();
+
+        /**
+         * 旋转桌布
+         */
+        if (isRotate) {
+            canvas.rotate((float) alpha, mCenterX, mCenterY);
+
+            alpha = Math.atan((mTouchX - mCenterX) / (mCenterY - mTouchY));
+            alpha = Math.toDegrees(alpha);
+            if (mTouchY > mCenterY) {
+                alpha = alpha + 180;
+            }
+            rotateCanvas(canvas);
+        }
+
 
         /**
          * 最外圈
          */
 
-        drawOutCircle(canvas, centerX, centerY);
+        drawOutCircle(canvas, mCenterX, mCenterY);
 
         /**
          * 小圆圈
          */
-        drawSmallCircle(canvas, centerX);
+        drawSmallCircle(canvas, mCenterX);
 
         /**
          * 文字
@@ -121,29 +158,92 @@ public class ClockView extends View {
          * 箭头中间圆圈
          */
         drawArrowCircle(canvas);
+
+    }
+
+    private void rotateCanvas(Canvas canvas) {
+        mMatrix.reset();
+        mCamera.save();
+        mCamera.rotateX(mCanvasRotateX);
+        mCamera.rotateY(mCanvasRotateY);
+        mCamera.getMatrix(mMatrix);
+        mCamera.restore();
+        mMatrix.preTranslate(-mCenterX, -mCenterY);
+        mMatrix.postTranslate(mCenterX, mCenterY);
+
+        canvas.concat(mMatrix);
+    }
+
+    @Override public boolean onTouchEvent(MotionEvent event) {
+        if (isRotate) {
+            float x = event.getX();
+            float y = event.getY();
+            mTouchX = x;
+            mTouchY = y;
+
+            switch (event.getActionMasked()) {
+                case MotionEvent.ACTION_DOWN: {
+                    rotateCanvasWhenMove(x, y);
+                    return true;
+                }
+                case MotionEvent.ACTION_MOVE: {
+                    rotateCanvasWhenMove(x, y);
+                    invalidate();
+                    return true;
+                }
+                case MotionEvent.ACTION_UP: {
+                    mCanvasRotateY = 0;
+                    mCanvasRotateX = 0;
+                    invalidate();
+                    return true;
+                }
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void rotateCanvasWhenMove(float x, float y) {
+        float dx = x - mCenterX;
+        float dy = y - mCenterY;
+
+        float percentX = dx / mCenterX;
+        float percentY = dy / mCenterY;
+
+        if (percentX > 1f) {
+            percentX = 1f;
+        } else if (percentX < -1f) {
+            percentX = -1f;
+        }
+        if (percentY > 1f) {
+            percentY = 1f;
+        } else if (percentY < -1f) {
+            percentY = -1f;
+        }
+
+        mCanvasRotateY = mCanvasMaxRotateDegree * percentX;
+        mCanvasRotateX = -(mCanvasMaxRotateDegree * percentY);
     }
 
     private void drawArrowCircle(Canvas canvas) {
-        canvas.drawCircle(centerX, centerY, smallCircleRadius * 2, arrowCirclePaint);
+        canvas.drawCircle(mCenterX, mCenterY, smallCircleRadius * 2, arrowCirclePaint);
     }
 
     private void drawArrow(Canvas canvas) {
-        arrowPath.moveTo(centerX, centerY - arrowOffsetY);
-        arrowPath.lineTo(centerX - arrowOffsetX, centerY);
-        arrowPath.lineTo(centerX, centerY + arrowOffsetY);
-        arrowPath.lineTo(centerX + arrowOffsetX, centerY);
-        arrowPath.lineTo(centerX, centerY - arrowOffsetY);
+        arrowPath.moveTo(mCenterX, mCenterY - arrowOffsetY);
+        arrowPath.lineTo(mCenterX - arrowOffsetX, mCenterY);
+        arrowPath.lineTo(mCenterX, mCenterY + arrowOffsetY);
+        arrowPath.lineTo(mCenterX + arrowOffsetX, mCenterY);
+        arrowPath.lineTo(mCenterX, mCenterY - arrowOffsetY);
         arrowPath.close();
         canvas.drawPath(arrowPath, arrowPaint);
     }
 
     private void drawText(Canvas canvas) {
-        canvas.drawText("N", centerX - smallCircleRadius / 2, lineLength, textPaint);
+        canvas.drawText("N", mCenterX - smallCircleRadius / 2, lineLength, textPaint);
     }
 
 
     private void drawSmallCircle(Canvas canvas, int centerX) {
-
         int smallCircleY = getPaddingTop() + lineLength + smallCircleRadius + smallCircleOffset;
         canvas.drawCircle(centerX, smallCircleY, smallCircleRadius, smallCirclePaint);
     }

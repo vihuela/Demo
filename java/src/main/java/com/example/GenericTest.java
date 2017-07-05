fe16090c907cc10cc80d073c0db674a76dab0d6e
ricky <ricky@worldgo.net>
2017-07-05 12:04:11 +0800

    update generic demo

--- java/src/main/java/com/example/GenericTest.java
+++ java/src/main/java/com/example/GenericTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class GenericTest {
     public static void main(String[] args) {
         //A->B->C
-        //super为了写入，右边是左边的父类
+        //super为了写入，右边是左边的父类【生产者】
         List<? super B> l12 = new ArrayList<B>();
         List<? super B> l13 = new ArrayList<C>();
         l13.add(new A());
@@ -19,12 +19,15 @@ public class GenericTest {
 //        l13.add(new B());ERROR
 
 
-        //extends为了读取，右边是左边的子类
+        //extends为了读取，右边是左边的子类【消费者】
         List<? extends B> l4 = new ArrayList<B>();
         List<? extends B> l3 = new ArrayList<A>();
 
 //        l2.add(new C());ERROR
         B b = l4.get(1);
+
+        //【既是生产者，又是消费者】
+        List<B> l5 = new ArrayList<B>();
 
     }
 
